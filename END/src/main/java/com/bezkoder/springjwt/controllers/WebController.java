3542f382a30bcb1b6bb98ff695dff12a60b686bf
Nam02-java <79495294+Nam02-java@users.noreply.github.com>
2023-08-25 10:46:59 +0700

    Update WebController.java

--- END/src/main/java/com/bezkoder/springjwt/controllers/WebController.java
+++ END/src/main/java/com/bezkoder/springjwt/controllers/WebController.java
@@ -1,10 +1,12 @@
 package com.bezkoder.springjwt.controllers;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,9 +25,14 @@ public class WebController {
     private static String female_voice = "//*[@id=\"voice_name_bin\"]/div[1]/label";
     private static File chosenFile = null;
     private static String fileName;
+    private static WebDriver driver;
+
+    private static String Vietnamese = "138";
+
+    private static String xpath_vietnameseToText = "138. Vietnamese (Vietnam) - VN";
 
     @GetMapping("/ttsfree")
-    private static void control_web(@RequestParam Map<String, String> params) throws InterruptedException {
+    public void test(@RequestParam Map<String, String> params) throws InterruptedException {
         String text = params.get("Text");
         String voice = params.get("Voice");
         fileName = params.get("FileName");
@@ -36,47 +43,58 @@ public class WebController {
         ChromeOptions options = new ChromeOptions();
         options.setExperimentalOption("debuggerAddress", "localhost:9222");
 
-        WebDriver driver = new ChromeDriver(options);
+        driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
 
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        //khởi tạo thao tác với lăn chuột
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        WebElement Element = driver.findElement(By.xpath("//*[@id=\"input_text\"]"));
 
-        Thread.sleep(5000);
+        js.executeScript("arguments[0].scrollIntoView();", Element);
+
         driver.findElement(By.xpath("//*[@id=\"input_text\"]")).clear();
 
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("//*[@id=\"input_text\"]")).sendKeys(text);
+        waitForElementToSendKeys(100, "//*[@id=\"input_text\"]", text);
 
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("//*[@id=\"select2-select_lang_bin-container\"]")).click();
-
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("/html/body/span/span/span[1]/input")).sendKeys("138");
-
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("/html/body/span/span/span[1]/input")).sendKeys(Keys.ENTER);
+        if (driver.findElement(By.xpath("//*[@id=\"select2-select_lang_bin-container\"]")).getText().equals(xpath_vietnameseToText)) {
+        } else {
+            driver.findElement(By.xpath("//*[@id=\"select2-select_lang_bin-container\"]")).click();
+            waitForElementToSendKeys(100, "/html/body/span/span/span[1]/input", Vietnamese);
+            driver.findElement(By.xpath("/html/body/span/span/span[1]/input")).sendKeys(Keys.ENTER);
+        }
 
         if (voice.equals("Male")) {
-            Thread.sleep(2000);
             driver.findElement(By.xpath(male_voice)).click();
         } else if (voice.equals("Female")) {
-            Thread.sleep(2000);
             driver.findElement(By.xpath(female_voice)).click();
         }
 
-        Thread.sleep(2000);
         driver.findElement(By.xpath("//*[@id=\"frm_tts\"]/div[2]/div[2]/div[1]/a")).click();
 
-        Thread.sleep(8000);
-
-        driver.findElement(By.xpath("//*[@id=\"progessResults\"]/div[2]/center[1]/div/a")).click();
+        js.executeScript("arguments[0].scrollIntoView();", Element); // thi thoảng web sẽ tự kéo xuống sau khi convert giọng nói nên dòng này sẽ có nhiệm vụ lăn chuột về nơi nút dowload hiển thị
+        waitForElementUnstable(5, 30, "//*[@id=\"progessResults\"]/div[2]/center[1]/div/a");
 
         Thread.sleep(2000);
-        getLastModified("C:\\Users\\Pc\\Downloads");
-        Thread.sleep(2000);
+        getLastModified("C:\\test\\");
         convert_file_name();
-        System.out.println("rename : " + chosenFile);
+    }
+
+
+    public void waitForElementToSendKeys(int seconds, String waitConditionLocator, String text) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(seconds));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(waitConditionLocator))).sendKeys(text);
+    }
+
+    public void waitForElementUnstable(int timeOut, int pollingEvery, String xpath) {
+        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(timeOut)).pollingEvery(Duration.ofSeconds(pollingEvery)).ignoring(NoSuchElementException.class, StaleElementReferenceException.class);
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpath)));
+    }
+
+
+    public void waitForElementToClick(int seconds, String waitConditionLocator, String text) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(seconds));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(waitConditionLocator))).click();
     }
 
     public static File getLastModified(String directoryFilePath) {
@@ -97,7 +115,8 @@ public class WebController {
     }
 
     public static void convert_file_name() {
-        File rename = new File("C:\\Users\\Pc\\Downloads\\" + fileName + ".mp3");
+        String file_path = "C:\\test\\";
+        File rename = new File(file_path + fileName + ".mp3");
 
         boolean flag = chosenFile.renameTo(rename);
 
@@ -109,4 +128,3 @@ public class WebController {
         }
     }
 }
-
