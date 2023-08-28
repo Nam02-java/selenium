50fd54860b2a1195c9ce82dd34a4fa6a74311cc7
Nam02-java <79495294+Nam02-java@users.noreply.github.com>
2023-08-28 11:45:09 +0700

    Update WebController.java

--- END/src/main/java/com/bezkoder/springjwt/controllers/WebController.java
+++ END/src/main/java/com/bezkoder/springjwt/controllers/WebController.java
@@ -33,7 +33,7 @@ public class WebController {
     private static String xpath_vietnameseToText = "138. Vietnamese (Vietnam) - VN";
 
     @GetMapping("/ttsfree")
-    public void ttsfreeAPI(@RequestParam Map<String, String> params) throws InterruptedException, IOException {.
+    public void ttsfreeAPI(@RequestParam Map<String, String> params) throws InterruptedException, IOException {
         String text = params.get("Text");
         String voice = params.get("Voice");
         fileName = params.get("FileName");
@@ -45,8 +45,6 @@ public class WebController {
         options.setExperimentalOption("debuggerAddress", "localhost:9222");
 
         driver = new ChromeDriver(options);
-
-        Thread.sleep(1000);
 
         driver.manage().window().maximize();
 
@@ -68,18 +66,18 @@ public class WebController {
         }
 
         if (voice.equals("Male")) {
-            driver.findElement(By.xpath(male_voice)).click();
+            waitForElementToClick(10, male_voice);
         } else if (voice.equals("Female")) {
-            driver.findElement(By.xpath(female_voice)).click();
+            waitForElementToClick(10, female_voice);
         }
 
         driver.findElement(By.xpath("//*[@id=\"frm_tts\"]/div[2]/div[2]/div[1]/a")).click();
 
-        Thread.sleep(1500);
+        Thread.sleep(2500);
 
         js.executeScript("arguments[0].scrollIntoView();", Element); // thi thoảng web sẽ tự kéo xuống sau khi convert giọng nói nên dòng này sẽ có nhiệm vụ lăn chuột về nơi nút dowload hiển thị
 
-        waitForElementUnstable(5, 30, "//*[@id=\"progessResults\"]/div[2]/center[1]/div/a");
+        waitForElementUnstable(5, 60, "//*[@id=\"progessResults\"]/div[2]/center[1]/div/a");
 
         Thread.sleep(2000);
         getLastModified("C:\\Users\\Pc\\Downloads\\");
@@ -101,7 +99,7 @@ public class WebController {
     }
 
 
-    public void waitForElementToClick(int seconds, String waitConditionLocator, String text) {
+    public void waitForElementToClick(int seconds, String waitConditionLocator) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(seconds));
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath(waitConditionLocator))).click();
     }
@@ -137,6 +135,7 @@ public class WebController {
             System.out.println("Operation Failed");
         }
     }
+}
 
 }
 
